5ed244de7e5635d22c428b7776a2224425611aa0
doongu <must1080@naver.com>
2024-04-19 22:10:23 +0900

    feat : 단일 조회 API 생성

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import Session
 
 from database.connection import get_db
 from database.orm import ToDo
-from database.repository import get_todos
+from database.repository import get_todos, get_todo_by_todo_id
+from schema.response import ToDoSchema, ListToDoResponse
 
 app = FastAPI()
 
@@ -40,14 +41,16 @@ todo_data = {
 def get_todos_handler(
         order: str | None = None,
         session: Session = Depends(get_db),
-):
+) -> ListToDoResponse:
 
     todos: List[ToDo] = get_todos(session=session)
 
     ret = list(todo_data.values())
     if order and order == "DESC":
         return ret[::-1]
-    return todos
+    return ListToDoResponse(
+        todos=[ToDoSchema.from_orm(todo) for todo in todos]
+    )
 
 class CreateToDoRequest(BaseModel):
     id: int
@@ -63,9 +66,10 @@ def create_todo_handler(request: CreateToDoRequest):
     return todo_data[request.id]
 
 @app.delete("/todos/{todo_id}", status_code=204)
-def delete_todo_handler(todo_id: int):
+def delete_todo_handler(todo_id: int,
+                        session: Session = Depends(get_db)):
 
-    todo = todo_data.pop(todo_id, None)
+    todo: ToDo | None = get_todo_by_todo_id(session=session, todo_id=todo_id)
     if todo:
         return
     raise HTTPException(status_code=404, detail="ToDo Not Found")
