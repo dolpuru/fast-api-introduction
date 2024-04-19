2786076dcec0b26554a1abe0dcc5642b6cefd439
doongu <must1080@naver.com>
2024-04-19 15:39:19 +0900

    feat : todos데이터 추가 및 get api 추가

--- src/main.py
+++ src/main.py
@@ -5,3 +5,29 @@ app = FastAPI()
 @app.get("/")
 def health_check_handler():
     return {"ping" : "pong"}
+
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "실전! FastAPI 섹션 0 수강",
+        "is-done": True,
+    },
+
+
+    2: {
+        "id": 2,
+        "contents": "실전! FastAPI 섹션 1 수강",
+        "is-done": True,
+    },
+
+
+    3: {
+        "id": 3,
+        "contents": "실전! FastAPI 섹션 2 수강",
+        "is-done": True,
+    },
+}
+
+@app.get("/todos")
+def get_todos_handler():
+    return list(todo_data.values())
